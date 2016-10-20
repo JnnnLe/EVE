1d7d88972a4682d3d7ab272e9f80e77b507fd27a
Jennifer Lê <jnnnle.sf@gmail.com>
2016-10-20 12:39:39 -0700

    Setup webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+	entry: [
+	  './client/index.js'
+	],
+	output: {
+		path: __dirname + '/client/',
+		publicPath: '/client/',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [{
+			exclude: /node_modules/,
+			loader: 'babel',
+			query: {
+				presets: ['react', 'es2015', 'stage-1']
+			}
+		}]
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	devServer: {
+		historyApiFallback: true,
+		contentBase: './'
+	}
+	
+};
