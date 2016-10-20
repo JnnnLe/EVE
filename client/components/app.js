51eea01e8a9a5bcff5d06ae9bd653c33e8a80ba4
Jennifer Lê <jnnnle.sf@gmail.com>
2016-10-20 14:44:41 -0700

    Created TimeAndGreeting component and exported to App.js

--- client/components/app.js
+++ client/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TimeAndGreeting from 'timeAndGreeting';
 
 class App extends Component {
 	render() {
---
+++ client/components/timeAndGreeting.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TimeAndGreeting extends Component {
+	render() {
+	  return (
+	  	<div>
+	  		<div>Time</div>
+	  		<div>Greeting</div>
+	  	</div>
+	  ) 
+	}
+
+};
+
+export default TimeAndGreeting;
