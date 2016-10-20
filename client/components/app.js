21063538c50a07138fe178f10c4ce59bd359c0b2
Jennifer Lê <jnnnle.sf@gmail.com>
2016-10-20 14:28:49 -0700

    Exporting the contents in App

--- client/components/app.js
+++ client/components/app.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 class App extends Component {
 	render() {
-		return <div>Starting project with Mr Meanie, yells alot - super butt farts!</div>
+		return <div>I hope that this works</div>
 	}
-}
+};
+
+export default App;
