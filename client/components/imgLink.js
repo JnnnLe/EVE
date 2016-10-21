da97e9afe469024ccfb639e3e3a6cb77e6b65de1
Jennifer Lê <jnnnle.sf@gmail.com>
2016-10-21 12:56:27 -0700

    Created component for image info + link

---
+++ client/components/imgLink.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ImgLink extends Component {
+	render() {
+		return (
+			<div className='imgLink'>Image Location and Link</div>
+		)
+	}
+};
+
+export default ImgLink;
