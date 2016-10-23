c316e0373d31df805171092deef722c72135d612
Jennifer Lê <jnnnle.sf@gmail.com>
2016-10-23 16:26:04 -0700

    Created todo component

--- client/components/app.js
+++ client/components/app.js
@@ -4,12 +4,14 @@ import Weather from './weather';
 import Links from './links';
 import Quote from './quote';
 import ImgLink from './ImgLink';
+import ToDo from './todo';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<Weather/>
+				<ToDo>
 				<TimeAndGreeting/>
 				<Links/>
 				<Quote/>
---
+++ client/components/todo.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class ToDo extends Component {
+	render() {
+		return (
+			<div className='toDoTitle'>ToDo:</div>
+			<div className='toDoList'>'Start listing...'></div>
+		)
+	}
+};
+
+export default ToDo;
